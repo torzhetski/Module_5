db90ce192f84b306850cbd9e5036e6112e19a6b8
Захар Торжецкий <torzhetski@mail.ru>
2022-08-20 09:43:47 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using static System.Console;
+class Program
+{
+    /// <summary>
+    /// разделение строки на слова с помощью встроенной библиотеки и функции string.Split()
+    /// </summary>
+    /// <param name="Text"></param>
+    /// <returns></returns>
+    static string[] Split(string Text)
+    {
+        return Text.Split( ' ' );
+
+    }
+    /// <summary>
+    /// разделение строки на слова без использования библиотек и функции string.Split()
+    /// </summary>
+    /// <param name="Text"></param>
+    /// <returns></returns>
+    static string[] SplitingWitoutSplitMethod(string Text)
+    {
+        int key = 1;
+        foreach (char e in Text)
+            if (e == ' ') key++;
+        string[] text= new string[key];
+        key = 0;
+        for(int i = 0; i < Text.Length;i++)
+        {
+            if (Text[i] == ' ') key++;
+            else text[key] += Convert.ToString(Text[i]);
+        }
+        return text;
+    }
+    /// <summary>
+    /// вывод на экран по содному слову
+    /// </summary>
+    /// <param name="Text"></param>
+    static void Print(string[] Text)
+    {
+        foreach (var e in Text)
+            WriteLine(e);
+    }
+    /// <summary>
+    /// основная функция
+    /// </summary>
+    static void Main()
+    {
+        string text = ReadLine();
+        var splitedText = Split(text);
+        Print(splitedText);
+        ReadKey(); WriteLine();
+        splitedText = SplitingWitoutSplitMethod(text);
+        Print(splitedText);
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+class Program
+{
+    /// <summary>
+    /// разделение строки на слова с помощью встроенной библиотеки и функции string.Split()
+    /// </summary>
+    /// <param name="Text"></param>
+    /// <returns></returns>
+    static string[] Split(string Text)
+    {
+        return Text.Split(new char[] { ' ' });
+
+    }
+    /// <summary>
+    /// размещение слов в обратном порядке
+    /// </summary>
+    /// <param name="Phrase"></param>
+    static void ReversPhrase(string Phrase)
+    {
+        var splitedPhrase=Split(Phrase);
+        //foreach (var word in splitedPhrase)
+        //    WriteLine(word);
+        string reversPhrase="";
+        for (int i = splitedPhrase.Length-1; i >=0; i--)
+        {
+            reversPhrase += splitedPhrase[i]+" ";
+        }
+        Write($"{reversPhrase}");
+    }
+    /// <summary>
+    /// основная функция
+    /// </summary>
+    static void Main()
+    {
+        string Text = ReadLine();
+        ReversPhrase(Text);
+    }
+}
